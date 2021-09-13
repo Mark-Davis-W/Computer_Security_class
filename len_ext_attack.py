2adcd7437b4bbae23cd9736086c6050b7d6f2895
Mark-Davis-W <1874026+Mark-Davis-W@users.noreply.github.com>
2021-09-13 12:38:43 -0600

    Length extension part should be done.

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-from urllib.parse import quote, urlparse
+from urllib.parse import quote, urlparse, urlunparse
 from pymd5 import md5, padding
 
 
@@ -26,6 +26,36 @@ if __name__ == '__main__':
 
     #################################
     # Your length extension code here
+    #Parsing up the url
+    query = url.query
+    firstPos = query.find("token=")
+    secondPos = query.find("&")
+    oldCommands = query[secondPos+1:]
+    newCommand = "&command=UnlockSafes"
+    #length of old commands plus known 8 byte password length
+    len_of_command = len(oldCommands) + 8
+    #bits of of the block based off the previous
+    bits = (len_of_command + len(padding(len_of_command*8)))*8
+    #extract the token hex
+    token = query[firstPos+6:secondPos]
+    #Initialize hash at current state
+    hStart = md5(state=bytes.fromhex(token), count=bits)
+    hStart.update(newCommand)
+    #replace the token with new token
+    query = query.replace(query[firstPos+6:secondPos],hStart.hexdigest())
+    #calculae padding and use quote to pass url safe
+    paddit = quote(padding(len_of_command*8))
+    #replace the whole backend after the token and first & with new data
+    query = query.replace(q[secondPos+1:], oldCommands + paddit + newCommand)
+    #replace the whole query in url with newly made query 
+    url = url._replace(query = query)
+
+    #finally print out unparsed url
+    print(urlunparse(url))
+
+
+
+
 
 
 
