9f2bb3e3369c9778e0d6d23c7786034383449995
F Claude <52762083+whoiscoos@users.noreply.github.com>
2021-10-27 17:47:18 -0600

    part02 - some progress -> now logging ip addresses to stdout

--- detector.py
+++ detector.py
@@ -7,7 +7,21 @@ import sys
 def process_pcap(pcap_fname):
     for pkt in PcapReader(pcap_fname):
         # Your code here
-        pass
+        if pkt.haslayer(Ether):
+            if pkt.haslayer(IP):
+                if pkt.haslayer(TCP):
+                    # check for 'SYN' packets
+                    if pkt[TCP].flags == 'S':
+                        # store source ip of SYN packet 
+                        ip_addr = pkt[IP].src
+                        print("SYN packet found..........: " + ip_addr)
+
+                    # check for 'SYN+ACK' packets
+                    if pkt[TCP].flags == 'SA': 
+                        # store source ip of 'SYN+ACK' packet 
+                        ip_addr = pkt[IP].src
+                        print("SYN+ACK packet found......: " + ip_addr)
+
 
 if __name__=='__main__':
     if len(sys.argv) != 2:
