2a748faea2e5d5e1fe63b94af83b1f8b23b6d856
riathakkar <riathakkar16@gmail.com>
2021-11-29 18:58:30 -0700

    Update test_solidity.py

--- test/test_solidity.py
+++ test/test_solidity.py
@@ -9,8 +9,8 @@ with open(sys.argv[1], "rb") as f:
     with open(sys.argv[2]) as contract:
         contract_source = contract.read()
         res = eth.get_contract_source_code(contract_source)
-        sha256_source_code = hashlib.sha256(res[0]['SourceCode'].encode('utf-8')).hexdigest()
-        sha256_attack_sol = hashlib.sha256(byte_file).hexdigest()
+        sha256_source_code = hashlib.sha256(res[0]['SourceCode'].encode('utf-8').replace(b'\r\n', b'\n').replace(b' ', b'')).hexdigest()
+        sha256_attack_sol = hashlib.sha256(byte_file.replace(b'\r\n', b'\n').replace(b' ', b'')).hexdigest()
 
         if(sha256_attack_sol == sha256_source_code):
             print("passed")
