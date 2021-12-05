023ca04d3fcf8313f5695394459f263c8d6bfc06
F Claude <52762083+whoiscoos@users.noreply.github.com>
2021-12-04 18:13:05 -0700

    ugh

--- attack.sol
+++ attack.sol
@@ -1,20 +1,16 @@
 pragma solidity ^0.6.0;
-//
-// This contract is vulnerable to having its funds stolen.
-// Written for ECEN 4133 at the University of Colorado Boulder: https://ecen4133.org/
-// (Adapted from ECEN 5033 w19)
-// SPDX-License-Identifier: WTFPL
-//
-// Happy hacking, and play nice! :)
 
-contract Vuln {
+contract Vuln 
+{
     mapping(address => uint256) public balances;
-    function deposit() public payable {
+    function deposit() public payable 
+    {
         // Increment their balance with whatever they pay
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public 
+    {
         // Refund their balance
         msg.sender.call.value(balances[msg.sender])("");
         // Set their balance to 0
@@ -24,7 +20,7 @@ contract Vuln {
 
 contract attack 
 {
-    address vuln_addr = 0x36A540E3A78084962B75E25877CfACf8846Be01o8;
+    address vuln_addr = 0x36A540E3A78084962B75E25877CfACf8846Be018;
     Vuln vuln_cont = Vuln(vuln_addr);
     uint256 count = 0;
 
